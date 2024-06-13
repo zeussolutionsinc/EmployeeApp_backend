dc4571554bfc1648ca20b3681d49dc935c2b2854
MuskanMallZeus <165341068+MuskanMallZeus@users.noreply.github.com>
2024-06-13 16:30:02 -0500

    Combined All Backend

---
+++ EmployeePortal/DTO/AdminEmpTimesheetDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeePortal.DTO
+{
+    public class AdminEmpTimesheetDTO
+    {
+        public Guid? EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+    }
+}
---
+++ EmployeePortal/DTO/AdminTimesheetDTO.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace EmployeePortal.DTO
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AdminTimesheetDTO
+    {
+        public Guid? EmployeeId { get; set; }
+        //public Guid? Approver { get; set; }
+        //public string EmployeeName { get; set; }
+
+        public string EmployeeName { get; set; }
+        public string ProjectName { get; set; }
+        public string ApprovalStatus { get; set; }
+       // public string ClientEmail { get; set; }
+         public DateOnly SubmissionDate { get; set; }
+        public List<ProjectDateHoursEntry> ProjectDateHours { get; set; } = new List<ProjectDateHoursEntry>();
+
+        // Additional properties or methods can be added here as needed.
+
+    }
+}
+
